840e0c23ede4af67da5f3ab670b6da455c2e42a4
cloud <stephenholy@163.com>
2021-03-04 17:11:45 +0800

    Netty心跳检测机制案例

---
+++ src/main/java/com/cloud/netty/heartbeat/HeartBeatServerHandler.java
@@ -0,0 +1,56 @@
+package com.cloud.netty.heartbeat;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * HeartBeatServerHandler
+ *
+ * @author: cloud
+ * @date: 2021/3/4 16:49
+ * @version: 1.0.0
+ */
+public class HeartBeatServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * @param ctx 上下文
+     * @param evt 事件
+     * @throws Exception
+     */
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+
+        if (evt instanceof IdleStateEvent) {
+
+            // 将evt向下转型 IdleStateEvent
+            IdleStateEvent event = (IdleStateEvent) evt;
+
+            String eventType;
+
+            switch (event.state()) {
+                case READER_IDLE:
+                    eventType = "读空闲";
+                    break;
+                case WRITER_IDLE:
+                    eventType = "写空闲";
+                    break;
+                case ALL_IDLE:
+                    eventType = "读写空闲";
+                    break;
+                default:
+                    eventType = "";
+            }
+
+            System.out.println(ctx.channel().remoteAddress() + "--超时时间--" + eventType);
+            System.out.println("服务器做相应处理..");
+
+            // 如果发生空闲，我们关闭通道
+            // ctx.channel().close();
+
+        }
+
+    }
+}
